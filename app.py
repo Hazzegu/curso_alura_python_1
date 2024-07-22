671b28472bd73a0001668713b39bf308870fa9e2
Gustavo Chaves <chaves.0@gmail.com>
2024-07-22 12:14:21 -0300

    Aula 1

---
+++ app.py
@@ -0,0 +1,16 @@
+# import this
+
+print('''
+░█▀▀▀█ ─█▀▀█ ░█▀▀█ ░█▀▀▀█ ░█▀▀█ 　 ░█▀▀▀ ▀▄░▄▀ ░█▀▀█ ░█▀▀█ ░█▀▀▀ ░█▀▀▀█ ░█▀▀▀█ 
+─▀▀▀▄▄ ░█▄▄█ ░█▀▀▄ ░█──░█ ░█▄▄▀ 　 ░█▀▀▀ ─░█── ░█▄▄█ ░█▄▄▀ ░█▀▀▀ ─▀▀▀▄▄ ─▀▀▀▄▄ 
+░█▄▄▄█ ░█─░█ ░█▄▄█ ░█▄▄▄█ ░█─░█ 　 ░█▄▄▄ ▄▀░▀▄ ░█─── ░█─░█ ░█▄▄▄ ░█▄▄▄█ ░█▄▄▄█\n''')
+
+print('Escolha uma opção: \n')
+print('1 - Cadastrar Restaurante')
+print('2 - Listar Restaurante')
+print('3 - Ativar Restaurante')
+print('4 - Sair \n')
+
+selected_option = input('Digite a opção escolhida: \n')
+
+print(f'Você escolheu a opção: {selected_option}')
