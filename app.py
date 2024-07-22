0c819c89400489683636c6636f8570fbd3e411b5
Gustavo Chaves <chaves.0@gmail.com>
2024-07-22 14:57:15 -0300

    Aula 3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # import this
 import os 
 
+restaurantes = ['sushibar','cebolinha food']
+
 def show_title():
     print('''
 ░█▀▀▀█ ─█▀▀█ ░█▀▀█ ░█▀▀▀█ ░█▀▀█ 　 ░█▀▀▀ ▀▄░▄▀ ░█▀▀█ ░█▀▀█ ░█▀▀▀ ░█▀▀▀█ ░█▀▀▀█ 
@@ -19,22 +21,58 @@ def exit_app():
 
     print('Finalizar App \n')
 
-def select_options():
-    selected_option = int(input('Digite a opção escolhida: \n'))
+def invalid_option():
+    print('Opção inválida \n')
 
-    match selected_option:
-        case 1:
-            print('Cadastrar Restaurante')
-        case 2:
-            print('Listar Restaurantes')
-        case 3:
-            print('Ativar Restaurante')
-        case 4:
-            exit_app()
-        case _:
-            print('Opção inválida')
+    return_to_main_menu()
+
+def select_options(): 
+    try:
+        selected_option = int(input('Digite a opção escolhida: \n'))
+
+        match selected_option:
+            case 1:
+                register_restaurant()
+            case 2:
+                show_restaurants()
+            case 3:
+                print('Ativar Restaurante')
+            case 4:
+                exit_app()
+            case _:
+                invalid_option()
+    except:
+        invalid_option()
+
+def register_restaurant():
+    show_subtitle('Cadastro de Restaurantes')
+    
+    new_restaurant = input('Digite o nome do novo restaurante: ')
+
+    restaurantes.append(new_restaurant)
+
+    print('\nRestaurante cadastrado com sucesso!')
+
+    return_to_main_menu()
+
+def show_restaurants():
+    show_subtitle('Lista de Restaurantes')
+    
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    return_to_main_menu()
+
+def return_to_main_menu():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
+def show_subtitle(text): 
+    os.system('cls')
+    print(f'\n{text}\n')
 
 def main():
+    os.system('cls')
     show_title()
     show_options()
     select_options()
